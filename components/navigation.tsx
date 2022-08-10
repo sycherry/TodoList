03a0b3ce14d21bcc80f18cb3d83801aad956954e
saya <saya@saya-mac-book.local>
2022-08-10 13:54:59 +0900

    minor clean up

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -4,26 +4,21 @@ import { NavigationContainer } from "@react-navigation/native";
 import { Authentication } from "../screens/authentication";
 import { Home } from "../screens/home";
 
-
 export type NavigatorParamList = {
   Authentication: any
   Home: any
 }
-
 export default function RootNavigation() {
   const Stack = createStackNavigator<NavigatorParamList>();
-
   const screenOptions = {
     headerShown: false,
   };
-
   return (
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName="Authentication" screenOptions={screenOptions}>
-          <Stack.Screen name="Authentication" component={Authentication} />
-          <Stack.Screen name="Home" component={Home} />
-        </Stack.Navigator>
-     </NavigationContainer> 
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Authentication" screenOptions={screenOptions}>
+        <Stack.Screen name="Authentication" component={Authentication} />
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
