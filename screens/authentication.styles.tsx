23e8505d6beaba059037610cd2f98c2766870466
saya <saya@saya-mac-book.local>
2022-08-10 13:55:23 +0900

    created authentication screen

---
+++ screens/authentication.styles.tsx
@@ -0,0 +1,26 @@
+import { StyleSheet } from 'react-native';
+import { palette } from "../styles/color"
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    paddingBottom: 60
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 28,
+  },
+  button: {
+    backgroundColor: palette.blue,
+    paddingHorizontal: 40,
+    paddingVertical: 18,
+    borderRadius: 30
+  },
+  buttonText: {
+    color: palette.white,
+    fontSize: 20,
+    fontWeight: "bold"
+  }
+});
--- screens/authentication.tsx
+++ screens/authentication.tsx
@@ -1,41 +1,66 @@
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import React, { FC } from 'react'
+import { Alert, Text, View, TouchableOpacity, Button, Linking, Platform } from 'react-native';
 import { AuthenticationProps } from './authentication.props';
+import * as LocalAuthentication from "expo-local-authentication";
+import { styles } from './authentication.styles';
 
-export const Authentication: React.FC<AuthenticationProps> = ({ navigation }) => {
-   const toHome = () => {
-        navigation.navigate('Home')
-    } 
-    return (
-        <View style={styles.container}>
-          <Text style={styles.title}>Set Authentication to Proceed</Text>
-          <TouchableOpacity style={styles.button} onPress={() => toHome() } >
-          <Text style={styles.buttonText}>Go to Settings</Text>
-          </TouchableOpacity>
-    
-        </View>
+export const Authentication: FC<AuthenticationProps> = ({ navigation }) => {
+
+  const checkSecurityLevel = async () => {
+    const SecurityLevel = await LocalAuthentication.getEnrolledLevelAsync();
+
+    // When no enrolled authentication in deveice, link to settings
+    if (SecurityLevel === 0) {
+      return Alert.alert(
+        'No enrolled authentication',
+        'Please set authentication',
+        [{ text: "OK", onPress: () => handleSetPasscode() }]
+      );
+    } else {
+      // When enrolled non-biometric authentication or biometric authentication
+      // move to authentication
+      handleAuthentication()
+    }
+  };
+
+  const handleSetPasscode = async () => {
+
+    const isBiometricAvailable = await LocalAuthentication.hasHardwareAsync();
+
+    // When a face or fingerprint scanner is available on the device, link to setting face or fingerprint.
+    if (isBiometricAvailable) {
+      // We can't provision (can't enable passcode lock) in xcode IOS Simulator.
+      // I tested using physical devices.
+      Platform.OS === 'ios'
+        ? Linking.openURL('App-Prefs:TOUCHID_PASSCODE')
+        : await Linking.sendIntent("android.settings.BIOMETRIC_ENROLL");
+    } else {
+      // When a face or fingerprint scanner is not available on the device, link to setting page
+      Platform.OS === 'ios'
+        ? Linking.openURL('App-Prefs:')
+        : await Linking.sendIntent("android.settings.SECURITY_SETTINGS");
+    }
+  }
+
+  const handleAuthentication = async () => {
+    const result = await LocalAuthentication.authenticateAsync({
+      promptMessage: "Please authenticate",
+      cancelLabel: "CANCEL",
+      fallbackLabel: "Wrong passcode"
+    });
+    if (result.success) {
+      navigation.navigate('Home')
+    } else {
+      LocalAuthentication.cancelAuthenticate();
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Set Authentication to Proceed</Text>
+      <TouchableOpacity testID="button" style={styles.button} onPress={() => checkSecurityLevel()} >
+        <Text style={styles.buttonText}>Go to Settings</Text>
+      </TouchableOpacity>
+    </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'flex-end',
-    paddingBottom: 60
-  },
-  title: {
-    fontSize: 24,
-    marginBottom: 28,
-  },
-  button: {
-    backgroundColor: '#0145A6',
-    paddingHorizontal: 40,
-    paddingVertical: 18,
-    borderRadius:30
-  },
-  buttonText: {
-    color: '#fff',
-    fontSize:20,
-    fontWeight: "bold"
-  }
-});
