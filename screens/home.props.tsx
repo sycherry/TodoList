70b42e1cbadfdce46b066c69de23ca2dde358485
saya <saya@saya-mac-book.local>
2022-08-24 14:29:47 +0900

    remove files

--- screens/home.props.tsx
+++
@@ -1,4 +0,0 @@
-import { NavigatorParamList } from "../components/Navigation";
-import { StackScreenProps } from "@react-navigation/stack";
-
-export type HomeProps = StackScreenProps<NavigatorParamList, "Home">;
